f27b4858568e991179cd14fea2d3b88d0f48ec4c
Youngerjesus <yjh930919@gmail.com>
2021-06-21 16:07:05 +0900

    Initiliaze

---
+++ src/main/java/me/study/effectivejava/EffectivejavaApplication.java
@@ -0,0 +1,13 @@
+package me.study.effectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectivejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectivejavaApplication.class, args);
+	}
+
+}
