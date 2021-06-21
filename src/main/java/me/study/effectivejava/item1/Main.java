639927c725007aef0b5d4b8cd0d6419c6cc2526d
Youngerjesus <yjh930919@gmail.com>
2021-06-21 18:44:57 +0900

    Add Item1

---
+++ src/main/java/me/study/effectivejava/item1/Main.java
@@ -0,0 +1,9 @@
+package me.study.effectivejava.item1;
+
+import java.util.EnumSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Boolean TRUE = Boolean.valueOf(true);
+    }
+}
